b69f12f7ddae80194de4929ec8f010353bd5d9f8
rangera <adam6860@vandals.uidaho.edu>
2013-12-11 15:41:41 -0800

    consoleclient improvement

--- support/consoleclient.py
+++ support/consoleclient.py
@@ -8,11 +8,17 @@ c = client.root
 #print "Games:"
 #for game in c.list_games()["response"]["games"]:
 #    print game["name"]
-
-#c.start_game("test1", "bob1")
+def startgame():
+    c.start_game("test1", "bob1")
     
-planets = c.get_state(object_type="Planet")#["response"]["Planet"]
-environs = c.get_state(object_type="Environ")#["response"]["Environ"]
-characters = c.get_state(object_type="Character")#["response"]["Character"]
-units = c.get_state(object_type="Unit")#["response"]["Unit"]
-stack = c.get_state(object_type="Stack")#["response"]["Stack"]
+def getstuff(values):
+    if values.count('p') > 0:
+        print c.get_state(object_type="Planet")["response"]["planet"]
+    if values.count('e') > 0:
+        print c.get_state(object_type="Environ")["response"]["environ"]
+    if values.count('c') > 0:
+        print c.get_state(object_type="Character")["response"]["character"]
+    if values.count('u') > 0:
+        print c.get_state(object_type="Unit")["response"]["unit"]
+    if values.count('s') > 0:
+        print c.get_state(object_type="Stack")["response"]["stack"]
