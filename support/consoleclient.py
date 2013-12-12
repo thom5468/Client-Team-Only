e012013154f03a3d4c50878168191202add366dd
rangera <adam6860@vandals.uidaho.edu>
2013-12-11 21:04:18 -0800

    changed consoleclient

--- support/consoleclient.py
+++ support/consoleclient.py
@@ -11,14 +11,14 @@ c = client.root
 def startgame():
     c.start_game("test1", "bob1")
     
-def getstuff(values):
-    if values.count('p') > 0:
+def getstuff(value):
+    if value = 'p':
         return c.get_state('test1', "Planet")["response"]["planet"]
-    if values.count('e') > 0:
+    if  value = 'e':
         return c.get_state('test1',"Environ")["response"]["environ"]
-    if values.count('c') > 0:
+    if  value = 'c':
         return c.get_state('test1', "Character")["response"]["character"]
-    if values.count('u') > 0:
+    if  value = 'u':
         return c.get_state('test1', "Unit")["response"]["unit"]
-    if values.count('s') > 0:
+    if  value = 's':
         return c.get_state('test1', "Stack")["response"]["stack"]
